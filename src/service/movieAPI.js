c7b7dd0eb5494024dff1d396a597c5825d2e9442
Artem Repko <a.repko33@gmail.com>
2024-12-20 16:31:06 +0200

    bug-fixing

--- src/service/movieAPI.js
+++ src/service/movieAPI.js
@@ -1,19 +1,22 @@
+
+
 import axios from "axios";
+
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjhlZmZiODEzNTFmZDc4NGE3YzRkNjE4MmI4YWI0OSIsIm5iZiI6MTczNDY0NTMzMS45NzksInN1YiI6IjY3NjQ5NjUzODBkY2Q4Zjc4ZjgyMjRjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jkgSGNgvKK8QTB75qdWHMpt2gXoqB0WwZjCcYC-mDls";
+
 const movieInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-
   headers: {
-    Authorization: "Bearer ${token}",
+    Authorization: `Bearer ${token}`,
   },
   params: {
     include_adult: false,
-    language: 'en-US',
+    language: "en-US",
   },
 });
 
 export const getTrendingMovies = async (time_window = "week") => {
-  const { data } = await movieInstance.get("/trending/movie/${time_window}", {
+  const { data } = await movieInstance.get(`/trending/movie/${time_window}`, {
     params: {
       page: 1,
     },
@@ -21,8 +24,8 @@ export const getTrendingMovies = async (time_window = "week") => {
   return data;
 };
 
-export const searchMovieByName = async movieName => {
-  const { data } = await movieInstance.get("/search/movie", {
+export const searchMovieByName = async (movieName) => {
+  const { data } = await movieInstance.get(`/search/movie`, {
     params: {
       query: movieName,
       page: 1,
@@ -31,15 +34,17 @@ export const searchMovieByName = async movieName => {
   return data;
 };
 
-export const fetchMovieDetails = async movie_id => {
-  const { data } = await movieInstance.get("/movie/${movie_id}");
+export const fetchMovieDetails = async (movie_id) => {
+  const { data } = await movieInstance.get(`/movie/${movie_id}`);
   return data;
 };
-export const fetchMovieCredits = async movie_id => {
-  const { data } = await movieInstance.get("/movie/${movie_id}/credits");
+
+export const fetchMovieCredits = async (movie_id) => {
+  const { data } = await movieInstance.get(`/movie/${movie_id}/credits`);
   return data;
 };
-export const fetchMovieReviews = async movie_id => {
-  const { data } = await movieInstance.get("/movie/${movie_id}/reviews");
+
+export const fetchMovieReviews = async (movie_id) => {
+  const { data } = await movieInstance.get(`/movie/${movie_id}/reviews`);
   return data;
 };
