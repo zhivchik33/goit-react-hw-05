d46a642b5ca2d5be7707f641ef98a40772f5c37f
Artem Repko <a.repko33@gmail.com>
2024-12-20 21:14:55 +0200

    Fix movie poster URL

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,3 +1,4 @@
+
 import css from "./MovieDetailsPage.module.css";
 import { useEffect, useRef, useState } from "react";
 import { Link, Outlet, useLocation, useParams } from "react-router-dom";
@@ -10,28 +11,30 @@ const MovieDetailsPage = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const { movieId } = useParams();
-  const backlink = useRef(location.state ?? '/'); 
+  const backlink = useRef(location.state ?? "/");
 
-
-  const defaultImg = "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
+  const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
   useEffect(() => {
     if (!movieId) return;
+
     const asyncWrapper = async () => {
       try {
         setIsLoading(true);
         setError(null);
         const requestData = await fetchMovieDetails(movieId);
         setMovie(requestData);
-      
       } catch {
-        setError('Something went wrong. Please try again later.');
+        setError("Something went wrong. Please try again later.");
       } finally {
         setIsLoading(false);
       }
     };
     asyncWrapper();
   }, [movieId]);
+
   return (
     <div>
       <div className={css.goBack}>
@@ -46,7 +49,7 @@ const MovieDetailsPage = () => {
               className={css.poster}
               src={
                 movie.poster_path
-                  ? "https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+                  ? `${BASE_IMAGE_URL}${movie.poster_path}`
                   : defaultImg
               }
               width={250}
@@ -59,7 +62,7 @@ const MovieDetailsPage = () => {
                 <p className={css.des}>{movie.overview}</p>
                 <h2 className={css.heading}>Genres</h2>
                 <p className={css.des}>
-                  {movie.genres.map(genre => genre.name).join(', ')}
+                  {movie.genres.map((genre) => genre.name).join(", ")}
                 </p>
               </div>
             </div>
